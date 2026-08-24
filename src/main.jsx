ed0a27f41cbc68ddfc99c14f11dbc4aee2efbf58
Ashraful Islam <theashrafislam@gmail.com>
2026-08-24 20:53:47 +0600

    tailwind css setup done also delete unnecearry files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <h1 className='text-red-500'>Hello I am ok</h1>
   </StrictMode>,
 )
